2ef1ff6b84a14e73536c57d351bbefa6aba172ef
Roland Krüger <info@rolandkrueger.info>
2016-05-10 20:16:43 +0200

    Initiale Projektstruktur des Beispielprojekts MediaLibrary

---
+++ Maven-Projekte/Kap05_MediaLibrary/src/main/java/org/vaadin/grundlagenbuch/MedialibraryApplication.java
@@ -0,0 +1,11 @@
+package org.vaadin.grundlagenbuch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedialibraryApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MedialibraryApplication.class, args);
+	}
+}
---
+++ Maven-Projekte/Kap05_MediaLibrary/src/test/java/org/vaadin/grundlagenbuch/MedialibraryApplicationTests.java
@@ -0,0 +1,18 @@
+package org.vaadin.grundlagenbuch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MedialibraryApplication.class)
+@WebAppConfiguration
+public class MedialibraryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
