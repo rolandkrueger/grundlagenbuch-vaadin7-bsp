633c47015e6d118a2dd530d5c0540a491b8f63c4
Roland Krüger <info@rolandkrueger.info>
2016-05-15 14:48:28 +0200

    Konfiguration des Scan-Paths für JPA Repositories

--- Maven-Projekte/Kap05_MediaLibrary/src/main/java/org/vaadin/grundlagenbuch/MedialibraryApplication.java
+++ Maven-Projekte/Kap05_MediaLibrary/src/main/java/org/vaadin/grundlagenbuch/MedialibraryApplication.java
@@ -2,8 +2,10 @@ package org.vaadin.grundlagenbuch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "org.vaadin.grundlagenbuch.repository")
 public class MedialibraryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MedialibraryApplication.class, args);
