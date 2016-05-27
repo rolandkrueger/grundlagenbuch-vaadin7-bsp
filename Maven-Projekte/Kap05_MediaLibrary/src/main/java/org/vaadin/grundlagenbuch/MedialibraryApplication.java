b6276268f7661e595685939cfe1842b693779e7f
Roland Krüger <info@rolandkrueger.info>
2016-05-27 14:22:39 +0200

    Konfiguration der Packages für Elasticsearch-Repositorien

--- Maven-Projekte/Kap05_MediaLibrary/src/main/java/org/vaadin/grundlagenbuch/MedialibraryApplication.java
+++ Maven-Projekte/Kap05_MediaLibrary/src/main/java/org/vaadin/grundlagenbuch/MedialibraryApplication.java
@@ -2,10 +2,12 @@ package org.vaadin.grundlagenbuch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "org.vaadin.grundlagenbuch.repository")
+@EnableElasticsearchRepositories(basePackages = "org.vaadin.grundlagenbuch.elasticsearch.repository")
 public class MedialibraryApplication {
   public static void main(String[] args) {
     SpringApplication.run(MedialibraryApplication.class, args);
