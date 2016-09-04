686d5433222c87cf24d3178d58e0551c15ce7de4
Roland Krüger <info@rolandkrueger.info>
2016-09-04 14:37:01 +0200

    Valo-Theme und Titel setzen

--- Maven-Projekte/Kap05_MediaLibrary/src/main/java/org/vaadin/grundlagenbuch/ui/ApplicationUI.java
+++ Maven-Projekte/Kap05_MediaLibrary/src/main/java/org/vaadin/grundlagenbuch/ui/ApplicationUI.java
@@ -1,14 +1,22 @@
 package org.vaadin.grundlagenbuch.ui;
 
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Title;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.ui.Label;
+import com.vaadin.ui.Component;
 import com.vaadin.ui.UI;
 
 @SpringUI
+@Theme("valo")
+@Title("MediaLibrary")
 public class ApplicationUI extends UI {
   @Override
   protected void init(VaadinRequest vaadinRequest) {
-    setContent(new Label("Hallo Welt!"));
+    setContent(buildContent());
+  }
+
+  private Component buildContent() {
+    return null;
   }
 }
